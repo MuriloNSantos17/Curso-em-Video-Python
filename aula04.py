b7a6b1c0687143bcebd183a0519a4af12bf16674
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2024-12-05 08:06:09 -0300

    Commit learning first commands
    Class 04, learning about input and output information using python

---
+++ aula04.py
@@ -0,0 +1,17 @@
+print(7+5);
+print('7'+'5');
+
+name = "Murilo Santos";
+age= 25;
+weight = 130
+
+nome = input('\nDigite seu nome: ')
+idade = input('Digite sua idade: ')
+peso = input('Digite o seu peso: ')
+
+print('\nEnglish',name,age,weight);
+
+print('Portuguese',nome,idade,peso)
+
+
+
